c1b057ec614269c2c253019cc20b72199d4b1926
James Adolfsson <adolfssonjames@gmail.com>
2021-11-26 17:56:33 +0100

    Start

---
+++ omdb-template/js/main.js
@@ -0,0 +1,24 @@
+/**
+ *  OMDb template
+ *	Documentation: http://www.omdbapi.com/
+ *  Generate an API key here: http://www.omdbapi.com/apikey.aspx
+ */
+
+
+/**
+* According to documentation, you need at least 2 parameters when calling the API http://www.omdbapi.com/
+* 1 Required parameter: apikey
+* 2 Required parameter: One of the following i=, t= or s=
+*
+* 
+* Example with parameter s=star trek
+* http://www.omdbapi.com/?apikey=[yourkey]&s=star trek
+*
+* Example with parameter s=star trek AND y=2020
+* http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&y=2020
+*
+* Example with parameter s=star trek AND type=series
+* http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&type=series
+*
+*/
+let url = 'http://www.omdbapi.com/?apikey=[yourkey]=star trek';
